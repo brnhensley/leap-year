69cd13e65b425fa138ed98cd7791e1c14a7f6ef6
Brian Hensley <brnhensley@gmail.com>
2019-04-02 22:47:21 -0700

    pass spec one, it returns false for everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-  $("form#leap-year").submit(funtion(event) {
+  $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
     $("#result").text(result);
   });
 });
+
+var leapYear = function(year) {
+  return false;
+};
